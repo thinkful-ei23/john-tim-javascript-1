2d8e224172faa55728394acef6667e86300ba605
Timothy Dao <timster78811@gmail.com>
2018-07-16 15:09:45 -0400

    i broke it on purpose

--- drills.js
+++ drills.js
@@ -1,7 +1,17 @@
-function whoAmI(name , age) {
-    let yearOfBirth = 2018-age;
-    console.log(`I was born in ${yearOfBirth}`);
-console.log("Hi my name is Tim and i am 29 years old")
+function yearOfBirth(age){
+    let yearOfBirth = 2018 - age;
+
+}
+
+function whoAmI(name, age) {
+    let yob = yearOfBirth(age);
+    console.log("Hi my name is Tim and i am 29 years old")
+    console.log(`I was born in ${yob}`);
+
 }
 
 whoAmI("Tim", 29);
+
+   
+    
+    
