d998329e9b4d22fe1020574c62488b6a535768d9
john <johnjacobcosgrove@gmail.com>
2018-07-16 16:03:48 -0400

    added try and catch

--- drills.js
+++ drills.js
@@ -1,26 +1,28 @@
 function yearOfBirth(age){
-    return 2018 - age;
-    
+    if(age <=0){
+        throw console.error('Age cannot be negative')
+    }
+    else{
+        return 2018 - age;
+    }
 }
 
 
 
 function whoAmI(name, age) {
     const yob = yearOfBirth(age);
-    if (age <= 0){
-        throw new Error('Age cannot be negative')
-    }
-    else{
+
         console.log("Hi my name is Tim and i am 29 years old");
         console.log(`I was born in ${yob}`);
-    }
+    
 }
 
 
 try{
     whoAmI("Tim", -5);
+    
 }
 catch(e){
-    console.log(e.messsage)
+
 }
     
