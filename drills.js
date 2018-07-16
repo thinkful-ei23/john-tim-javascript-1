f7ec65a656fa03f2884a732444b18168fb5b56cb
john <johnjacobcosgrove@gmail.com>
2018-07-16 15:53:34 -0400

    working

--- drills.js
+++ drills.js
@@ -1,17 +1,26 @@
 function yearOfBirth(age){
-    let yearOfBirth = 2018 - age;
-
+    return 2018 - age;
+    
 }
+
+
 
 function whoAmI(name, age) {
-    let yob = yearOfBirth(age);
-    console.log("Hi my name is Tim and i am 29 years old")
-    console.log(`I was born in ${yob}`);
-
+    const yob = yearOfBirth(age);
+    if (age <= 0){
+        throw new Error('Age cannot be negative')
+    }
+    else{
+        console.log("Hi my name is Tim and i am 29 years old");
+        console.log(`I was born in ${yob}`);
+    }
 }
 
-whoAmI("Tim", 29);
 
-   
-    
+try{
+    whoAmI("Tim", -5);
+}
+catch(e){
+    console.log(e.messsage)
+}
     
